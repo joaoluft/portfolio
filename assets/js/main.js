d60efe27284c68d20f54ad393273e0fd4320e9eb
João Luft <57741250+joaoluft@users.noreply.github.com>
2022-10-29 01:11:13 -0300

    #feat adicionado modais e conteúdo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,69 @@
 // Scroll ao clicar em um HREF com ID
 document.querySelectorAll('a[href^="#"]').forEach(a => {
-    a.addEventListener('click', function (e) {
-        e.preventDefault();
-        let href = this.getAttribute("href");
-        let elem = document.querySelector(href)||document.querySelector("a[name="+href.substring(1, href.length)+"]");
+  a.addEventListener('click', function (e) {
+    e.preventDefault();
+    let href = this.getAttribute("href");
+    let elem = document.querySelector(href) || document.querySelector("a[name=" + href.substring(1, href.length) + "]");
 
-        window.scroll({
-            top: elem.offsetTop, 
-            left: 0, 
-            behavior: 'smooth' 
-        });
+    window.scroll({
+      top: elem.offsetTop,
+      left: 0,
+      behavior: 'smooth'
     });
+  });
 });
 
 // Tela de carregamento
-window.onload = function(){
-    var preloader = document.querySelector('.preloader');
-  
-    var startTime = new Date().getTime();
-    function fadeOut(){
-      var passedTime = new Date().getTime() - startTime;
-      var opacity = Math.max(250 / (250 - passedTime), 0);
-      preloader.style.opacity = opacity;
-      if(opacity){
-        setTimeout(fadeOut, 0);
-      }
-    }
-    setTimeout(fadeOut, 0);
+function loadingOut() {
+  document.querySelector('.preloader').classList.toggle("loadingOut");
+};
+
+window.addEventListener('load', loadingOut);
+
+
+// Modal
+let currentModal = "";
+
+let openmodal = document.querySelectorAll('.modal-open')
+for (let i = 0; i < openmodal.length; i++) {
+  openmodal[i].addEventListener('click', function (event) {
+    event.preventDefault()
+    currentModal = openmodal[i]["dataset"]["modal"];
+    toggleModal(currentModal);
+  })
+}
+
+const closeButtons = document.querySelectorAll('.modal-close, .modal-overlay');
+
+for (const closeButton of closeButtons) {
+  closeButton.addEventListener('click', function handleClick(event) {
+    event.preventDefault();
+    toggleModal(currentModal);
+    currentModal = "";
+  });
+}
+
+document.onkeydown = function (evt) {
+  evt = evt || window.event
+  let isEscape = false
+
+  if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc")
+  } else {
+    isEscape = (evt.key === 27)
   }
+
+  if (isEscape && document.body.classList.contains('modal-active')) {
+    toggleModal(currentModal);
+    currentModal = "";
+  }
+};
+
+function toggleModal(id) {
+  console.log(id);
+  const body = document.querySelector('body');
+  const modal = document.querySelector("." + id);
+  modal.classList.toggle('opacity-0');
+  modal.classList.toggle('pointer-events-none');
+  body.classList.toggle('modal-active');
+}
